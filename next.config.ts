8a050ab850438bca2b32aa8e007a395299681c00
developer2520 <berkino2099@gmail.com>
2025-11-07 19:58:41 +0500

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // ESLint configuration is now handled via .eslintrc.json
-  // The eslint option in next.config.ts is deprecated
-  experimental: {
-    turbo: {
-      root: __dirname,
-    },
-  },
+  /* config options here */
 };
 
 export default nextConfig;
